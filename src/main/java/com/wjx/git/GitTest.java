b389d0d988091e4590113159640aa323f8423c68
xiang <YABEE168@qq.com>
2021-09-02 17:22:49 +0800

    V1.0

---
+++ src/main/java/com/wjx/git/GitTest.java
@@ -0,0 +1,14 @@
+package com.wjx.git;
+
+/**
+ * @author : wjx
+ * @describe ：测试git
+ * @create : 2021-02-17:17
+ * @Since : version-1.0
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("Hello Git!");
+
+    }
+}
